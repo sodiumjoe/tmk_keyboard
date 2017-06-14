321550ed73b298a6fa6fcdcb7cc98201c71132c0
Joe Moon <joe@xoxomoon.com>
2017-06-14 00:59:21 -0700

    fix layout

--- keyboard/fc660c/unimap_emu.c
+++ keyboard/fc660c/unimap_emu.c
@@ -3,12 +3,14 @@
 #include "hook.h"
 #include "fc660c.h"
 
+/* id for user defined functions */
+enum function_id {
+    ESCX,
+};
 
-#define AC_L3       ACTION_LAYER_MOMENTARY(3)
-#define AC_L4       ACTION_LAYER_MOMENTARY(4)
-#define AC_LS_2     ACTION_LAYER_MODS(2, MOD_LSFT)
-#define AC_RS_2     ACTION_LAYER_MODS(2, MOD_RSFT)
-#define AC_TGL1     ACTION_LAYER_TOGGLE(1)
+#define AC_L1       ACTION_LAYER_MOMENTARY(1)
+#define AC_1CTL     ACTION_LAYER_MODS(1, MOD_LCTL)
+#define AC_ESCX     ACTION_FUNCTION_TAP(ESCX)
 
 // emulates FC660C default keymap
 #ifdef KEYMAP_SECTION_ENABLE
@@ -17,49 +19,31 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] __attribute__ ((section ("
 const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
 #endif
     [0] = KMAP(
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS,
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL,
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,
-        LS_2,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RS_2,UP,
-        LCTL,LGUI,LALT,          SPC,                     RALT,RCTL,L3,  LEFT,DOWN,RGHT
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     VOLU,
+        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     VOLD,
+        1CTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,UP,
+        LCTL,LALT,LGUI,          SPC,                     RGUI,RALT,L1,  LEFT,DOWN,RGHT
     ),
     [1] = KMAP(
-        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS,
-        TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL,
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,
-        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,UP,
-        LCTL,LGUI,LALT,          SPC,                     RALT,RCTL,L4,  LEFT,DOWN,RGHT
-    ),
-    [2] = KMAP(
-        GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,    TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,ESCX,TRNS,TRNS,    MUTE,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,TRNS,
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS,TRNS,TRNS
-    ),
-    [3] = KMAP(
-        GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,     TRNS,
-        CAPS,TGL1,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,TRNS,TRNS,TRNS,     TRNS,
-        TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,TRNS,TRNS,     TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END, PGDN,TRNS,          TRNS,PGUP,
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,APP, L3,  HOME,PGDN,END
-    ),
-    [4] = KMAP(
-        ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,     TRNS,
-        CAPS,TGL1,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,TRNS,TRNS,TRNS,     TRNS,
-        TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,TRNS,TRNS,     TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,END, PGDN,TRNS,          TRNS,PGUP,
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,APP, L4,  HOME,PGDN,END
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          CAPS,PGUP,
+        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,L1,  HOME,PGDN,END
     ),
 };
 
-
-void hook_layer_change(uint32_t layer_state)
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-    // lights LED on Insert when layer 1 is enabled
-    if (layer_state & (1L<<1)) {
-        PORTB &= ~(1<<5);
-    } else {
-        PORTB |=  (1<<5);
+    switch (id) {
+        case ESCX:
+            if (record->event.pressed) {
+                unregister_mods(MOD_BIT(KC_LCTL));
+                register_code(KC_ESC);
+                unregister_code(KC_ESC);
+                register_mods(MOD_BIT(KC_LCTL));
+            }
+            break;
     }
 }
